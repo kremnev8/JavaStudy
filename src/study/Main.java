db2494d5ba2ae0796b7daba8e1115edb84a5bd3b
Ilya <123>
2020-09-05 14:05:23 +0300

    Task 1 done

--- src/study/Main.java
+++ src/study/Main.java
@@ -1,9 +1,36 @@
 package study;
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
+
+
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello world");
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Choose which program to run(0-4): ");
+        int prgID = 0;
+        try {
+             prgID = Integer.parseInt(reader.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        switch (prgID) {
+            case 0:{
+                int[] array = {1, 3, 5, 7, 11, 13};
+                int sum = 0;
+
+                for (int i = 0; i < array.length; i++){
+                    sum += array[i];
+                }
+                System.out.println("Array sum: " + sum);
+            }
+        }
     }
 }
