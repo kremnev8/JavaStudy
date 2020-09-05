1323c7ead5db2e1c08f4e3fb03943a4ae316c828
kremnev8 <kremnev8super@yandex.ru>
2020-09-05 21:09:28 +0300

    Cleanup

--- src/study/Main.java
+++ src/study/Main.java
@@ -1,15 +1,8 @@
 package study;
 
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.text.DecimalFormat;
-import java.util.Random;
 
 public class Main {
-
-
 
     public static void main(String[] args) {
 	// write your code here
