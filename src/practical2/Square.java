7e66069c478d1d97f2b680ec1da7f81d00c23fb9
kremnev8 <kremnev8super@yandex.ru>
2020-09-12 15:19:20 +0300

    Add folder

--- src/practical2/Square.java
+++ src/practical2/Square.java
@@ -2,30 +2,9 @@ package practical2;
 
 import Util.IShape;
 
-public class Square implements IShape {
-
-    private float a;
+public class Square extends Rect {
 
     public Square(float a) {
-
-        this.a = a;
-    }
-
-    @Override
-    public float GetPerimeter() {
-
-        return 4 * a;
-    }
-
-    @Override
-    public float GetArea() {
-
-        return a * a;
-    }
-
-    @Override
-    public float[] GetCenter() {
-
-        return new float[]{a / 2, a / 2};
+        super(a, a);
     }
 }
