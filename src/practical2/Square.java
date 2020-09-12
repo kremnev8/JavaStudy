c7f90409edbc808a7b960f28e13e6f968e02d2a5
kremnev8 <kremnev8super@yandex.ru>
2020-09-12 15:28:09 +0300

    Revert "Add folder"
    This reverts commit 7e66069c

--- src/practical2/Square.java
+++ src/practical2/Square.java
@@ -2,9 +2,30 @@ package practical2;
 
 import Util.IShape;
 
-public class Square extends Rect {
+public class Square implements IShape {
+
+    private float a;
 
     public Square(float a) {
-        super(a, a);
+
+        this.a = a;
+    }
+
+    @Override
+    public float GetPerimeter() {
+
+        return 4 * a;
+    }
+
+    @Override
+    public float GetArea() {
+
+        return a * a;
+    }
+
+    @Override
+    public float[] GetCenter() {
+
+        return new float[]{a / 2, a / 2};
     }
 }
